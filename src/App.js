17896e2182119906c3f586a62af1f3aa61acfa3e
shiba_1234 <shibajisahu121@gmail.com>
2023-03-20 23:00:07 +0530

    button design implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Shibaji</h1>
+      <div className="calc-container">
+        <div id="screen">
+          <span>432</span>
+        </div>
+        <div className="buttons-container">
+          <button className="func-btn">C</button>
+          <button className="func-btn">+/-</button>
+          <button className="func-btn">%</button>
+          <button className="func-btn">/</button>
+          <button className="func-btn">7</button>
+          <button className="func-btn">8</button>
+          <button className="func-btn">9</button>
+          <button className="func-btn">*</button>
+          <button className="func-btn">4</button>
+          <button className="func-btn">5</button>
+          <button className="func-btn">6</button>
+          <button className="func-btn">-</button>
+          <button className="func-btn">1</button>
+          <button className="func-btn">2</button>
+          <button className="func-btn">3</button>
+          <button className="func-btn">+</button>
+          <button className="func-btn">0</button>
+          <button className="func-btn">.</button>
+          <button className="func-btn">=</button>
+        </div>
+      </div>
     </div>
   );
 }
